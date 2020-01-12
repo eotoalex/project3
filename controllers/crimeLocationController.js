0947842d88774fff4020b7b5ee0b19a1b5569e60
Alex Puello <apuello88@yahoo.com>
2020-01-11 20:37:23 -0500

    added the date, crime committed, perpetrators race, perpetrators age, etc to the database info, but I need to get it to render correctly before putting the information in the info window to render on the map on hover or click.

--- controllers/crimeLocationController.js
+++ controllers/crimeLocationController.js
@@ -11,28 +11,43 @@ module.exports = {
         const CrimeLocation = new CrimeLocations({
             id: new mongoose.Types.ObjectId(),
             latitude: req.body.latitude,
-            longitude: req.body.longitude
+            longitude: req.body.longitude,
         })
-        axios.get("https://data.cityofnewyork.us/resource/uip8-fykc.json").then(function(response){
+        axios.get("https://data.cityofnewyork.us/resource/uip8-fykc.json")
+        .then(function(response){
         
         for (let i = 0; i < 10; i++){
                 let results = {};
-
                 let latitude = response.data[i].latitude;
                 let longitude = response.data[i].longitude;
+                let date = response.data[i].arrest_date;
+                let ageGroup = response.data[i].age_group;
+                let sex = response.data[i].perp_sex;
+                let race = response.data[i].perp_race;
+                let offence = response.data[i].ofns_desc;
+
 
                 results.latitude = latitude;
                 results.longitude = longitude;
-                CrimeLocations.create(results).then(function(dbCrime){
+                results.date = date;
+                results.ageGroup = ageGroup;
+                results.sex = sex;
+                results.race = race;
+                results.offence = offence;
+
+                CrimeLocation.create(results).then(function(dbCrime){
                     console.log(dbCrime);
                 }).catch(function(err){
                     console.log(err);
                 })
+                
             }
             
         }).catch(function(err){
             if (err) throw err;
         })
+        
     }
+
 
 }
