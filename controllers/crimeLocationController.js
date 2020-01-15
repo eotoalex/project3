9f55e8f51c10c084f819506b3630e591ab554b60
Alex Puello <apuello88@yahoo.com>
2020-01-14 23:01:33 -0500

    the infowindow now displays the crime committed, the date, which needs to me converted by momentJS, the Sex of the perpetrator, and the race of the individual involved in the crime. the info window mechanics have to be debugged, but after the moment JS business is resolved I will be moving to restablishing a functional display of certain crimes by type and a functional destination routing system.

--- controllers/crimeLocationController.js
+++ controllers/crimeLocationController.js
@@ -3,18 +3,20 @@ const axios = require('axios')
 // Need to devine mongoose some where.
 
 module.exports = {
+
     getCrimeData: function (req, res) {
         CrimeLocations.find({})
         .then((result) => {
+            console.log("result length ", result.length)
             res.json(result);
         });
     },
 
     addCrimeDataToDB: function(req, res) {
-        // This does not seem to be affecting the model input for data. (Still needs testing.)
+        // // This variable stops the database from creating copies of already existing data. I will id the data then use that id later to distinguish betweenw what goes into the database in the future.
 
         // const CrimeLocation = new CrimeLocations({
-        //     // id: new mongoose.Types.ObjectId(),
+        //     id: new mongoose.Types.ObjectId(),
         //     latitude: req.body.latitude,
         //     longitude: req.body.longitude,
         //     date:req.body.date,
@@ -23,36 +25,33 @@ module.exports = {
         axios.get("https://data.cityofnewyork.us/resource/uip8-fykc.json")
         .then(function(response){
         
-        for (let i = 0; i < 200; i++){
-            let results = {};
-            let latitude = response.data[i].latitude;
-            let longitude = response.data[i].longitude;
-            let date = response.data[i].arrest_date;
-            let ageGroup = response.data[i].age_group;
-            let sex = response.data[i].perp_sex;
-            let race = response.data[i].perp_race;
-            let offence = response.data[i].ofns_desc;
+            for (let i = 0; i < 10; i++){
+                let results = {};
+                let latitude = response.data[i].latitude;
+                let longitude = response.data[i].longitude;
+                let date = response.data[i].arrest_date;
+                let ageGroup = response.data[i].age_group;
+                let sex = response.data[i].perp_sex;
+                let race = response.data[i].perp_race;
+                let offence = response.data[i].ofns_desc;
+                let arrestKey = response.data[i].arrest_key;
 
-            // Here the data from the crime data set are seperated by type and put into the results object to be sent through to database.
-            results.latitude = latitude;
-            results.longitude = longitude;
-            results.date = date;
-            results.age_group = ageGroup;
-            results.sex = sex;
-            results.race = race;
-            results.offence = offence;
+                // Here the data from the crime data set are seperated by type and put into the results object to be sent through to database.
+                results.latitude = latitude;
+                results.longitude = longitude;
+                results.date = date;
+                results.age_group = ageGroup;
+                results.sex = sex;
+                results.race = race;
+                results.offence = offence;
+                results.arrest_key = arrestKey;
 
-            CrimeLocations.create(results).then(function(dbCrime){
-                console.log(dbCrime);
-            }).catch(function(err){
-                console.log(err);
-            })
-            
-            }
-            
-        })
-        
+                CrimeLocations.create(results).then(function(dbCrime){
+                    console.log(dbCrime);
+                }).catch(function(err){
+                    console.log(err);
+                }) 
+            };   
+        });  
     }
-
-
-}
+};
